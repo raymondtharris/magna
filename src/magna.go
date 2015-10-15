2a207f8dc33d2aad0bb31790c5362252ef01773c
Tim Harris <raymondtimothyharris@gmail.com>
2015-10-14 22:37:38 -0700

    Beginning Docker and starting Manga Project.

---
+++ src/magna.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+)
+
+type MagnaNode struct{
+  Index int
+  Value string
+  Weight int
+}
+
+
+func displayInput(){
+  fmt.Println("What would you like to explore?")
+  //Take in input from the user.
+}
+
+func main() {
+  fmt.Println("Welcome to Magna")
+}
