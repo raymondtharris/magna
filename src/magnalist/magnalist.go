9038c427f29a214769686576a6a92b133e069541
Tim Harris <raymondtimothyharris@gmail.com>
2015-10-18 14:55:42 -0700

    List implementation nearly finished. Need to just make a search function.

--- src/magnalist/magnalist.go
+++ src/magnalist/magnalist.go
@@ -22,14 +22,34 @@ func (mList magnaList) Push(newListNode *magnaListNode) {
 }
 
 func (mList magnaList) Pop() *magnaListNode {
-	mList.Length--
+	if mList.Tail == mList.Head {
+		returnNode := mList.Head
+		mList.Tail = nil
+		mList.Head = nil
+		mList.Length = 0
+		return returnNode
+	} else if mList.Tail != nil {
+		currentNode := mList.Head
+		var returnNode *magnaListNode
+		for currentNode != mList.Tail {
+			if currentNode.Next != mList.Tail {
+				currentNode = currentNode.Next
+			} else {
+				returnNode = currentNode.Next
+				currentNode.Next = nil
+				mList.Tail = currentNode
+			}
+		}
+		mList.Length--
+		return returnNode
+	}
 	return nil
 }
 
 func (mList magnaList) isEmpty() bool {
 	if mList.Head == nil {
 		return true
-	} else {
-		return false
 	}
+	return false
+
 }
