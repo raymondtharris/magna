2ab247898a298198f245d864a87e918b559a7c70
Tim Harris <raymondtimothyharris@gmail.com>
2015-10-17 22:47:16 -0700

    Initial build of list is almost finished. Need to just implement a few features and then it should be done.

--- src/magnalist/magnalist.go
+++ src/magnalist/magnalist.go
@@ -1,6 +1,7 @@
 package magnalist
 
 type magnaListNode struct {
+	Next *magnaListNode
 }
 
 type magnaList struct {
@@ -9,14 +10,26 @@ type magnaList struct {
 	Length int
 }
 
-func (mList magnaList) Push() {
+func (mList magnaList) Push(newListNode *magnaListNode) {
+	if mList.Head == nil {
+		mList.Head = newListNode
+		mList.Tail = mList.Head
+	} else {
+		mList.Tail.Next = newListNode
+		mList.Tail = newListNode
+	}
 	mList.Length++
 }
 
 func (mList magnaList) Pop() *magnaListNode {
+	mList.Length--
 	return nil
 }
 
 func (mList magnaList) isEmpty() bool {
-	return false
+	if mList.Head == nil {
+		return true
+	} else {
+		return false
+	}
 }
