d999fb31b868343f26b57a27b02c268c23ec5644
Tim Harris <raymondtimothyharris@gmail.com>
2015-10-19 19:53:57 -0700

    Moving list to the graph
    Had an import cycle. Fixed by moving the list package to the graph
    package.

--- magnagraph/magnagraph.go
+++ magnagraph/magnagraph.go
@@ -1,8 +1,8 @@
 package magnagraph
 
 import (
-	"fmt"
-	"magna/magnalist"
+	//"fmt"
+	//"magna/magnalist"
 )
 //Graph is comething
 type Graph struct {
@@ -13,4 +13,68 @@ type Node struct {
 	Index  int    //Inex of the node
 	Value  string //String value for the particular node
 	Weight int    // Weight of the node in relation to the others
+}
+
+//Node for a list
+type node struct {
+	Next *node
+	//Payload *magnagraph.Node
+}
+
+//List data structure
+type list struct {
+	Head   *node
+	Tail   *node
+	Length int
+}
+
+//Push inserts a Node at the end of the List
+func (mList list) Push(newNode *node) {
+	if mList.Head == nil {
+		mList.Head = newNode
+		mList.Tail = mList.Head
+	} else {
+		mList.Tail.Next = newNode
+		mList.Tail = newNode
+	}
+	mList.Length++
+}
+
+//Pop removes a Node from the end of the List
+func (mList list) Pop() *node {
+	if mList.Tail == mList.Head {
+		returnNode := mList.Head
+		mList.Tail = nil
+		mList.Head = nil
+		mList.Length = 0
+		return returnNode
+	} else if mList.Tail != nil {
+		currentNode := mList.Head
+		var returnNode *node
+		for currentNode != mList.Tail {
+			if currentNode.Next != mList.Tail {
+				currentNode = currentNode.Next
+			} else {
+				returnNode = currentNode.Next
+				currentNode.Next = nil
+				mList.Tail = currentNode
+			}
+		}
+		mList.Length--
+		return returnNode
+	}
+	return nil
+}
+
+func (mList list) isEmpty() bool {
+	if mList.Head == nil {
+		return true
+	}
+	return false
+
+}
+
+//Search will look for a Node and return in from a list
+func (mList list) Search(nodeToFind *Node) *node{
+	return nil
 }
