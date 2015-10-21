32639718210599a3dd26dca436f18c716f9036c2
Tim Harris <raymondtimothyharris@gmail.com>
2015-10-20 19:27:14 -0700

    List to Queue
    switched the list to a queue.

--- magna.go
+++ magna.go
@@ -57,5 +57,4 @@ func main() {
 	//for {
 	whatToSearch()
 	//}
-	fmt.Println("temp")
 }
--- magnagraph/magnagraph.go
+++ magnagraph/magnagraph.go
@@ -2,10 +2,12 @@ package magnagraph
 
 import (
 	//"fmt"
-	//"magna/magnalist"
 )
 //Graph is comething
 type Graph struct {
+	NumberOfVerticies int
+	NumberOfEdges int
+	ADJList []Node
 }
 
 //Node is something
@@ -15,66 +17,52 @@ type Node struct {
 	Weight int    // Weight of the node in relation to the others
 }
 
-//Node for a list
+//Node for a queue
 type node struct {
 	Next *node
-	//Payload *magnagraph.Node
+	Payload *Node
 }
 
-//List data structure
-type list struct {
+//Queue data structure
+type queue struct {
 	Head   *node
 	Tail   *node
 	Length int
 }
 
 //Push inserts a Node at the end of the List
-func (mList list) Push(newNode *node) {
-	if mList.Head == nil {
-		mList.Head = newNode
-		mList.Tail = mList.Head
+func (mQueue queue) Enqueue(newNode *node) {
+	if mQueue.Head == nil {
+		mQueue.Head = newNode
+		mQueue.Tail = mQueue.Head
 	} else {
-		mList.Tail.Next = newNode
-		mList.Tail = newNode
+		mQueue.Tail.Next = newNode
+		mQueue.Tail = newNode
 	}
-	mList.Length++
+	mQueue.Length++
 }
 
-//Pop removes a Node from the end of the List
-func (mList list) Pop() *node {
-	if mList.Tail == mList.Head {
-		returnNode := mList.Head
-		mList.Tail = nil
-		mList.Head = nil
-		mList.Length = 0
-		return returnNode
-	} else if mList.Tail != nil {
-		currentNode := mList.Head
-		var returnNode *node
-		for currentNode != mList.Tail {
-			if currentNode.Next != mList.Tail {
-				currentNode = currentNode.Next
-			} else {
-				returnNode = currentNode.Next
-				currentNode.Next = nil
-				mList.Tail = currentNode
-			}
-		}
-		mList.Length--
-		return returnNode
+//Pop removes a Node from the end of the queue
+func (mQueue queue) Dequque() *node {
+	if mQueue.Head != nil {
+		nodeToReturn := mQueue.Head
+		mQueue.Head = mQueue.Head.Next
+		nodeToReturn.Next = nil
+		mQueue.Lenth--
+		return nodeToReturn
 	}
 	return nil
 }
 
-func (mList list) isEmpty() bool {
-	if mList.Head == nil {
+func (mQueue queue) isEmpty() bool {
+	if mQueue.Head == nil {
 		return true
 	}
 	return false
 
 }
 
-//Search will look for a Node and return in from a list
-func (mList list) Search(nodeToFind *Node) *node{
+//Search will look for a Node and return in from a queue
+func (mQueue queue) Search(nodeToFind *Node) *node{
 	return nil
 }
