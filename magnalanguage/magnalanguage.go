9dfec78512c63b449b188f839996e81bc284253c
Tim Harris <raymondtimothyharris@gmail.com>
2015-12-05 22:17:20 -0800

    phase two
    Conditions for phase two have been added. Moving to phase 3 next.

--- magnalanguage/magnalanguage.go
+++ magnalanguage/magnalanguage.go
@@ -190,6 +190,72 @@ func phaseTwo(aStem string) string {
 		aStem := alliRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-4]+"al")
 		fmt.Println(aStem)
 	}
+	entliRegexp := regexp.MustCompile(".*[aeiou].entli$")
+	if len(entliRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := entliRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-5]+"ent")
+		fmt.Println(aStem)
+	}
+	eliRegexp := regexp.MustCompile(".*[aeiou].eli$")
+	if len(eliRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := eliRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-3]+"e")
+		fmt.Println(aStem)
+	}
+	ousliRegexp := regexp.MustCompile(".*[aeiou].ousli$")
+	if len(ousliRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := ousliRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-5]+"ous")
+		fmt.Println(aStem)
+	}
+	izationRegexp := regexp.MustCompile(".*[aeiou].ization$")
+	if len(izationRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := izationRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-7]+"ize")
+		fmt.Println(aStem)
+	}
+	ationRegexp := regexp.MustCompile(".*[aeiou].ation$")
+	if len(ationRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := ationRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-5]+"ate")
+		fmt.Println(aStem)
+	}
+	atorRegexp := regexp.MustCompile(".*[aeiou].ator$")
+	if len(atorRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := atorRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-4]+"ate")
+		fmt.Println(aStem)
+	}
+	alismRegexp := regexp.MustCompile(".*[aeiou].alism$")
+	if len(alismRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := alismRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-5]+"al")
+		fmt.Println(aStem)
+	}
+	ivenessRegexp := regexp.MustCompile(".*[aeiou].iveness$")
+	if len(ivenessRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := ivenessRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-7]+"ive")
+		fmt.Println(aStem)
+	}
+	fulnessRegexp := regexp.MustCompile(".*[aeiou].fulness$")
+	if len(fulnessRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := fulnessRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-7]+"ful")
+		fmt.Println(aStem)
+	}
+	ousnessRegexp := regexp.MustCompile(".*[aeiou].ousness$")
+	if len(ousnessRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := ousnessRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-7]+"ous")
+		fmt.Println(aStem)
+	}
+	alitiRegexp := regexp.MustCompile(".*[aeiou].aliti$")
+	if len(alitiRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := alitiRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-5]+"al")
+		fmt.Println(aStem)
+	}
+	ivitiRegexp := regexp.MustCompile(".*[aeiou].iviti$")
+	if len(ivitiRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := ivitiRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-5]+"ive")
+		fmt.Println(aStem)
+	}
+	bilitiRegexp := regexp.MustCompile(".*[aeiou].biliti$")
+	if len(bilitiRegexp.FindAllString(aStem, 1)) > 0 {
+		aStem := bilitiRegexp.ReplaceAllString(aStem, aStem[0:len(aStem)-6]+"ble")
+		fmt.Println(aStem)
+	}
+
 	return aStem
 }
 func phaseThree(aStem string) string {
