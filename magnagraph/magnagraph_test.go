fa2246ea287905346645547f30bb62c9d92c477e
Tim Harris <raymondtimothyharris@gmail.com>
2015-12-13 21:10:31 -0800

    More testing files
    Adding more testing to all the packages in magna.

---
+++ magnagraph/magnagraph_test.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+  "fmt"
+  "testing"
+)
+
+
+func TestListNode(t *testing.T){
+
+}
+
+func TestList(t *testing.T){
+
+}
+
+
+func TestNode(t *testing.T){
+
+}
+
+
+func TestGraph(t *testing.T){
+
+}
---
+++ magnalanguage/magnalanguage_test.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+  "fmt"
+  "testing"
+)
+
+func TestPorters(t *testing.T){
+
+  //Load file to read from
+  //Read file
+  // split words
+  //run wors through Porters
+
+}
+
+func TestTokenize(t *testing.T){
+
+}
