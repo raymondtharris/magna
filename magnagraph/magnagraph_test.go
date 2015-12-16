c7f84455b70056adb1ef50af16deb0f41762bcc2
Tim Harris <raymondtimothyharris@gmail.com>
2015-12-15 21:27:50 -0800

    Basic graph insert test.
    Added a small test for inserting vertices to the graph. Need to add
    some edges also.

--- magnagraph/magnagraph_test.go
+++ magnagraph/magnagraph_test.go
@@ -27,5 +27,15 @@ func TestNode(t *testing.T){
 
 
 func TestGraph(t *testing.T){
-
+  words := string[]{"the", "world", "farther", "places", "trusting", "higher", "max", "light"}
+  nodes := Node[]{}
+  for _, word := range words { //Change to get a random word to add.
+    anode := Node{1, word, 0, nil, 0, ""}
+    nodes = append(nodes, anode)
+  }
+  aGraph := Graph{0,0, nil, false}
+  for _, aNode := range nodes {
+    aGraph.AddVertex(*aNode)
+  }
+  fmt.Println(aGraph)
 }
