a32d3a26a351ecdf30ccc3950f1a1f093388c398
Tim Harris <raymondtimothyharris@gmail.com>
2015-12-16 20:31:29 -0800

    queue testing
    Added testing for the queue.

--- magnagraph/magnagraph_test.go
+++ magnagraph/magnagraph_test.go
@@ -7,11 +7,37 @@ import(
 
 
 func TestListNode(t *testing.T){
-
+  words := string[]{"the", "world", "farther", "places", "trusting", "higher", "max", "light"}
+  nodes := Node[]{}
+  for _, word := range words { //Change to get a random word to add.
+    anode := Node{1, word, 0, nil, 0, ""}
+    nodes = append(nodes, anode)
+  }
+  qNodes := node[]{}
+  for _, aNode := range nodes {
+    qNode := node{nil, *aNode}
+    qNodes = append(qNodes, qNode)
+  }
+  fmt.Println(lNodes)
 }
 
-func TestList(t *testing.T){
-
+func TestQueue(t *testing.T){
+  words := string[]{"the", "world", "farther", "places", "trusting", "higher", "max", "light"}
+  nodes := Node[]{}
+  for _, word := range words { //Change to get a random word to add.
+    anode := Node{1, word, 0, nil, 0, ""}
+    nodes = append(nodes, anode)
+  }
+  qNodes := node[]{}
+  for _, aNode := range nodes {
+    qNode := node{nil, *aNode}
+    qNodes = append(qNodes, qNode)
+  }
+  aQueue := queue{nil, nil, 0}
+  for _, qnode := range qNodes{
+    aQueue.Enqueue(*qnode)
+  }
+  fmt.Println(aQueue)
 }
 
 
