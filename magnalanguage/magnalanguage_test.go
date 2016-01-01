dcd204e96d1e4938b249d9f89a002e7eb50a747c
Tim Harris <raymondtimothyharris@gmail.com>
2016-01-01 15:01:18 -0800

    Processing test
    Processing test started uses all the processing steps that have been
    implemented.

--- magnalanguage/magnalanguage_test.go
+++ magnalanguage/magnalanguage_test.go
@@ -43,5 +43,21 @@ func TestTokenize(t *testing.T){
   fmt.Println(tokenArray)
 }
 func TestProcessing(t *testing.T){
-
+  sentences := []string{"the world is a crazy place we live in.", "What is the fascination with all this stuff out here.", "Making the world a better place will help us all in the end.", "I want to go to far away places to see new things.", "To be trusting is a very daring thing.", "What is this crazy golang thing about?", "Blast it to the max.", "The light side is strong in this one.", "Do you like hockey?", "I will fly away one day to a distant place.", "We have found an ally in you.", "To play these games you will need more controllers", "It is fun to roll around in the grass."}
+  nodes := []Node
+  for _, aSentence := range words {
+    aQueryObject := MagnaQueryObject{magnauser.User{"Wallace Mathers", 98239842773}, aSentence}
+    nodes = append(nodes, aQueryObject)
+  }
+  fmt.Println(nodes)
+  tokenArray := []magnagraph.Node{}
+  for _, anode := range nodes {
+    tempNode := TokenizeQuery(anode)
+    tokenArray = append(tokenArray, tempNode)
+  }
+  fmt.Println(tokenArray)
+  for _, pNode := range tokenArray {
+    ProcessNode(ProcessNode)
+  }
+  fmt.Println(tokenArray)
 }
