6bf31181239e57dac8413799283677efd6bcf86c
Tim Harris <raymondtimothyharris@gmail.com>
2016-01-05 20:34:34 -0800

    commenting on some code
    working out some framing for more expansion of code.

--- magnalanguage/magnalanguage_test.go
+++ magnalanguage/magnalanguage_test.go
@@ -23,6 +23,7 @@ func TestPorters(t *testing.T){
   }
 }
 func TestMeasure(t *testing.T){
+  //Measure Test uses an array of words and calculates the measure of each word.
   words := []string{"the", "world", "farther", "places", "trusting", "higher", "max", "light"}
   for _, aWord := range words {
     meas := FindMeasure(aWord)
@@ -30,6 +31,7 @@ func TestMeasure(t *testing.T){
   }
 }
 func TestTokenize(t *testing.T){
+  //Tokenize test takes in an array of sentences and then places them in to queryObjects and then tokenizes each query object.
   sentences := []string{"the world is a crazy place we live in.", "What is the fascination with all this stuff out here.", "Making the world a better place will help us all in the end.", "I want to go to far away places to see new things.", "To be trusting is a very daring thing.", "What is this crazy golang thing about?", "Blast it to the max.", "The light side is strong in this one.", "Do you like hockey?", "I will fly away one day to a distant place.", "We have found an ally in you.", "To play these games you will need more controllers", "It is fun to roll around in the grass."}
   nodes := []MagnaQueryObject{}
   for _, aSentence := range sentences{
@@ -46,6 +48,7 @@ func TestTokenize(t *testing.T){
   fmt.Println(tokenArray)
 }
 func TestProcessing(t *testing.T){
+  //Processing test does the beginning of the tokenize test and then processes all of the nodes.
   sentences := []string{"the world is a crazy place we live in.", "What is the fascination with all this stuff out here.", "Making the world a better place will help us all in the end.", "I want to go to far away places to see new things.", "To be trusting is a very daring thing.", "What is this crazy golang thing about?", "Blast it to the max.", "The light side is strong in this one.", "Do you like hockey?", "I will fly away one day to a distant place.", "We have found an ally in you.", "To play these games you will need more controllers", "It is fun to roll around in the grass."}
   nodes := []MagnaQueryObject{}
   for _, aSentence := range sentences {
@@ -60,7 +63,7 @@ func TestProcessing(t *testing.T){
     }
   }
   for _, pNode := range tokenArray {
-    ProcessNode(&pNode)
+    ProcessNode(&pNode)//Does not return the correct node processed even though it goes through all the steps. Need to fix.
   }
   fmt.Println(tokenArray)
 }
