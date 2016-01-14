d7113ed32b903f8a3f22da31fcfdf0473e27a535
Tim Harris <raymondtimothyharris@gmail.com>
2016-01-13 19:50:08 -0800

    DAG functions
    redid some graph functions for the DAG.

--- magnabayes/magnabayes.go
+++ magnabayes/magnabayes.go
@@ -31,26 +31,29 @@ func (mDAG DAG) AddVertex(newVertex *Node) {
 
 //AddEdgeBetween function inserts an edge between two vertecies in the graph. This is done
 //by adding a Neighbor to the nodes.
-func (mDAG Graph) AddEdgeBetween(vertex1 *Node, andVertex2 *Node, withCosts []int) {
-	mGraph.AddVertex(vertex1)
-	mGraph.AddVertex(andVertex2)
+func (mDAG DAG) AddEdgeBetween(vertex1 *Node, andVertex2 *Node, withCosts []int) {
+	mDAG.AddVertex(vertex1)
+	mDAG.AddVertex(andVertex2)
 	if len(withCosts) > 1 {
 		vertex1.AddNeighbor(andVertex2, withCosts[1])
-		if mGraph.IsDirected {
-			andVertex2.AddNeighbor(vertex1, withCosts[0])
-		}
 	} else {
 		vertex1.AddNeighbor(andVertex2, withCosts[0])
-		if mGraph.IsDirected {
-			andVertex2.AddNeighbor(vertex1, withCosts[0])
-		}
 	}
 }
 
 //findVertex function searches through  ADJList for a node. Will be switched to a binary search.
-func (mGraph Graph) findVertex(vertexToFind *Node) *Node {
-	for _, aNode := range mGraph.ADJList {
+func (mDAG DAG) findVertex(vertexToFind *Node) *Node {
+	for _, aNode := range mDAG.ADJList {
 		if &aNode == vertexToFind {
+			return &aNode
+		}
+	}
+	return nil
+}
+
+func (mDAG DAG) findVertexByWord(aWord string) *Node {
+	for _, aNode := range mDAG.ADJList {
+		if &aNode.Value == aWord {
 			return &aNode
 		}
 	}
