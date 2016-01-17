8cf5b94c44abebd366a47f218fb9e9e5465e73fd
Tim Harris <raymondtimothyharris@gmail.com>
2016-01-16 19:45:03 -0800

    bayes initial tests
    wrote out some of the tests for the bayes package for the DAG.

--- magnabayes/magnabayes_test.go
+++ magnabayes/magnabayes_test.go
@@ -7,26 +7,44 @@ import(
 )
 testWords := []string{"the", "world", "farther", "places", "trusting", "higher", "max", "light"}
 testNodes := []magnagraph.Node{}
+findStrings := []string{"farther", "world", "trusting", "light", "winner", "max", "alien"}
 
 func buildTestNodes(){
-  for _, aWord :=  range testWords {
-
+  testNodes = []magnagraph.Node{}
+  for index, aWord :=  range testWords {
+    newNode := magnagraph.Node{index, aWord, 1, nil, -1, ""}
+    testNodes = append(testNodes, newNode)
   }
 }
 
 
 func TestDAG(t *testing.T){
-
+  buildTestNodes()
 }
 
 func TestAddVertex(t *testing.T){
-
+  buildTestNodes()
+  mDAG := DAG{0,0,[]magnagraph.Node{}}
+  for _, aNode := range testNodes {
+    mDAG.AddVertex(aNode)
+  }
 }
 
 func TestAddVertecies(t *testing.T){
-
+  buildTestNodes()
+  mDAG := DAG{0,0,[]magnagraph.Node{}}
+  mDAG.ADDVertecies(testNodes)
 }
 
 func TestFindByString(t *testing.T){
-
+  buildTestNodes()
+  mDAG := DAG{0,0,[]magnagraph.Node{}}
+  mDAG.ADDVertecies(testNodes)
+  foundNodes := []magnagraph.Node{}
+  for _, aString := range findStrings {
+    foundVal := mDAG.findVertexByWord(aString)
+    if foundVal != nil {
+      foundNodes = append(foundNodes, foundVal)
+    }
+  }
 }
